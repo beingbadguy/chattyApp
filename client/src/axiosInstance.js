f50556c126104d78d49b0cbc17b186aa5e55c177
Aman Kumar <authorisedaman@gmail.com>
2025-01-26 18:47:02 +0530

    added

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://chattyapp-gy71.onrender.com",
-  // baseURL: "http://localhost:8000",
+  // baseURL: "https://chattyapp-gy71.onrender.com",
+  baseURL: "http://localhost:8000",
   withCredentials: true,
 });
 export default axiosInstance;
