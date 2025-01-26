9fe9f339ee41179ed57afca6499d38850e30a89c
Aman Kumar <authorisedaman@gmail.com>
2025-01-26 18:17:52 +0530

    added

--- server/src/config/socket.js
+++ server/src/config/socket.js
@@ -5,7 +5,11 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, { cors: { origin: ["http://localhost:5173"] } });
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", ["https://chattyapp-gy71.onrender.com/"]],
+  },
+});
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
